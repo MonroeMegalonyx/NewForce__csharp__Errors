71a4478c32281d43362f340f5c2d63cb6b43da67
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-04-20 10:11:22 -0400

    Added calculator class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace csharp_errors
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Calculator calculator = new Calculator();
+      int answer = calculator.Divide(42, 0);
+      Console.WriteLine($"The answer is {answer}");
     }
+  }
 }
